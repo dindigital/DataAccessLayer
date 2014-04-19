26bbf95e3b1b10968d123c4597d7f5845875a1c9
Jair Cueva Junior <junior@dindigital.com>
2014-04-18 21:22:53 -0300

    Introduçao ao TDD com PHPUnit

---
+++ tests/library/Din/DataAccessLayer/InsertTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Din\DataAccessLayer;
+
+class InsertTest extends \PHPUnit_Framework_TestCase
+{
+
+  public function testTrueIsTrue ()
+  {
+    $foo = true;
+    $this->assertTrue($foo);
+
+  }
+
+}
