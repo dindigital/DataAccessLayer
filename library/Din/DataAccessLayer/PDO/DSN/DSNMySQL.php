eadfdc9edfb2caae9a838e8e561e7905e73ba81f
Jair Cueva Júnior <junior@dinhost.com.br>
2024-07-09 15:49:13 -0300

    Muda charset para utf8mb4
    Dessa forma, estamos dando suporte aos emojis que são caracteres de 64 bits

--- library/Din/DataAccessLayer/PDO/DSN/DSNMySQL.php
+++ library/Din/DataAccessLayer/PDO/DSN/DSNMySQL.php
@@ -5,6 +5,6 @@ namespace Din\DataAccessLayer\PDO\DSN;
 class DSNMySQL extends AbstractDSN implements iDSN
 {
 
-  protected $_dsn = 'mysql:dbname={$schema};host={$host};charset=utf8';
+  protected $_dsn = 'mysql:dbname={$schema};host={$host};charset=utf8mb4';
 
 }
